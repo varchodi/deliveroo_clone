32106815f5c22be6bc7ca08a3e2379cfbf29ee84
varchodi <rauschenrobert@gmail.com>
2023-09-23 11:46:27 +0300

    bottom sheet package installed

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
       // Required for expo-router
-      'expo-router/babel',
+      "expo-router/babel",
+      // add reanimated plugin
+      "react-native-reanimated/plugin",
     ],
   };
 };
